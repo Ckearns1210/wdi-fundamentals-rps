5d131f7c26c8fde52a0f2c2eb154049e8fc92109
Colin Kearns <ckearns1210@gmail.com>
2015-09-23 13:43:17 -0400

    Finished Game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -33,34 +33,33 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    if ((playerMove = "rock") && (computerMove = "rock")) {
-        winner = "tie;"
-    }
-     else if(playerMove = "rock" && computerMove = "paper") {
-        winner = "computer";
-    }
-    else if(playerMove = "rock" && computerMove = "scissors") {
-        winner = "player";
-    }
-    else if(playerMove = "paper" && computerMove = "rock") {
-        winner = "player";
-    }
-    else if(playerMove = "paper" && computerMove = "paper") {
+    if ((playerMove === "rock") && (computerMove === "rock")) {
         winner = "tie";
     }
-    else if(playerMove = "paper" && computerMove = "scissors") {
+     else if(playerMove === "rock" && computerMove === "paper") {
         winner = "computer";
     }
-    else if(playerMove = "scissors" && computerMove = "rock") {
-        winner = "computer";
-    }
-    else if(playerMove = "scissors" && computerMove = "paper") {
+    else if(playerMove === "rock" && computerMove === "scissors") {
         winner = "player";
     }
-    else if(playerMove = "scissors" && computerMove = "scissors") {
+    else if(playerMove === "paper" && computerMove === "rock") {
+        winner = "player";
+    }
+    else if(playerMove === "paper" && computerMove === "paper") {
         winner = "tie";
     }
-}
+    else if(playerMove === "paper" && computerMove === "scissors") {
+        winner = "computer";
+    }
+    else if(playerMove === "scissors" && computerMove === "rock") {
+        winner = "computer";
+    }
+    else if(playerMove === "scissors" && computerMove === "paper") {
+        winner = "player";
+    }
+    else if(playerMove === "scissors" && computerMove === "scissors") {
+        winner = "tie";
+    }
     return winner;
 }
 
@@ -68,8 +67,39 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+
+    // This function should continue to play Rock Paper Scissors until either the
+    // player or the computer has won five times.
+    // After each 'round', display some text in the console indicating who played
+    // what, who won, and what the current scoreboard looks like.
+    // For example,
+    //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+    //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    // YOUR CODE HERE 
+
+   
+
+
+    while ((playerWins < 5) && (computerWins < 5)) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove, computerMove);
+        getWinner(playerMove, computerMove);
+
+    if (winner === "player") {
+        playerWins += 1;
+    }
+    else if (winner === "computer")  {
+        computerWins +=1;
+    }
+
+   
+    console.log('Player chose ' + playerMove + ' ' + 'while Computer chose ' + computerMove);
+    console.log(winner + ' ' + 'is the winner!');
+    console.log('The score is currently ' + playerWins + ' for the player and ' + computerWins + ' for the computer.' );
+
+    console.log[playerWins, computerWins];
 }
+}
+playToFive();
 
